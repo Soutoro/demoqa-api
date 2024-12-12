4cace54df9b2821c74151c62f37638741bd6e62c
Yaroslav Vorobev <ssoutoro@gmail.com>
2024-12-12 20:21:48 +0500

    Добавление алюра

--- src/test/java/tests/AllBooksTests.java
+++ src/test/java/tests/AllBooksTests.java
@@ -1,18 +1,21 @@
 package tests;
 
 
+import io.qameta.allure.restassured.AllureRestAssured;
 import org.junit.jupiter.api.Test;
 
+import static io.qameta.allure.Allure.step;
 import static io.restassured.RestAssured.*;
 import static org.hamcrest.Matchers.equalTo;
 
 
-public class AllBooksTests  extends BaseTest{
+public class AllBooksTests extends BaseTest {
 
     @Test
     public void checkBook() {
 
         given()
+                .filter(new AllureRestAssured())
                 .when()
                 .get("/BookStore/v1/Books")
                 .then()
@@ -24,6 +27,7 @@ public class AllBooksTests  extends BaseTest{
     public void checkBook1() {
 
         given()
+                .filter(new AllureRestAssured())
                 .when()
                 .get("/BookStore/v1/Books")
                 .then()
@@ -35,10 +39,23 @@ public class AllBooksTests  extends BaseTest{
     public void checkBook2() {
 
         given()
+                .filter(new AllureRestAssured())
                 .when()
                 .get("/BookStore/v1/Books")
                 .then()
                 .body("books.findAll {it.pages > 400}.pages.size()", equalTo(2));
 
     }
+
+//    @Test
+//    public void checkBook3() {
+//
+//        step("Тест", () -> {given()
+//                .filter(new AllureRestAssured())
+//                .when()
+//                .get("/BookStore/v1/Books")
+//                .then()
+//                .body("books.size()", equalTo(8));});
+//    }
+
 }
